27339d5ebd0f8942446a6ccff598479a63d79c40
RieN 7z <zrien7@gmail.com>
2025-05-24 08:54:58 +0800

    Initial Commit

---
+++ DVD ScreenSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DVD ScreenSaver
+//
+//  Created by RieN7 on 2025-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DVD ScreenSaver/DVD_ScreenSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVD_ScreenSaverApp.swift
+//  DVD ScreenSaver
+//
+//  Created by RieN7 on 2025-05-24.
+//
+
+import SwiftUI
+
+@main
+struct DVD_ScreenSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
